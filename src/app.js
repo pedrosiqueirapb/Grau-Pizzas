621a6109341de59c7a2921572e840df3ee92a44d
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2022-09-04 16:42:48 -0300

    Desenvolvendo site - dia 04

--- src/app.js
+++ src/app.js
@@ -1,83 +1,39 @@
-let btn = document.getElementById("btn").addEventListener('click', function mostrarMais(){
-    document.querySelector(".cardapio").innerHTML = `<h1><a name="Cardapio"></a>Cardápio<hr class="line-one"></h1>
-    <div class="container">
-        <div class="row">
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Frango com catupiry</h5>
-                </div>
-                <img src="imgs/frango-catupiry.png" alt="" class="frango-catupiry">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Calabresa</h5>
-                </div>
-                <img src="imgs/calabresa.png" alt="" class="calabresa">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Portuguesa</h5>
-                </div>
-                <img src="imgs/portuguesa.png" alt="" class="portuguesa">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Marguerita</h5>
-                </div>
-                <img src="imgs/marguerita.png" alt="" class="marguerita">
-            </div>
-        </div>
-        <div class="row">
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Quatro queijos</h5>
-                </div>
-                <img src="imgs/quatro-queijos.png" alt="" class="quatro-queijos">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Milho com bacon</h5>
-                </div>
-                <img src="imgs/milho-bacon.png" alt="" class="milho-bacon">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Pepperoni</h5>
-                </div>
-                <img src="imgs/pepperoni.png" alt="" class="pepperoni">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Chocolate</h5>
-                </div>
-                <img src="imgs/chocolate.png" alt="" class="chocolate">
-            </div>
-        <div class="row">
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Banana</h5>
-                </div>
-                <img src="imgs/banana.png" alt="" class="banana">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Bacon</h5>
-                </div>
-                <img src="imgs/bacon.png" alt="" class="bacon">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Lombo</h5>
-                </div>
-                <img src="imgs/lombo.png" alt="" class="lombo">
-            </div>
-            <div id="infosPizza" class="col-3">
-                <div class="saborPizza">
-                    <h5>🍴 Mussarela</h5>
-                </div>
-                <img src="imgs/mussarela.png" alt="" class="mussarela">
-            </div>
-        </div>
-    </div>
-    <button id="btn-Menos" type="button" class="btn btn-danger">- Mostrar menos</button>`;
+/* Botões "+ Mostrar mais" e "- Mostrar menos" da section Cardápio */
+
+let btnMais = document.querySelector("#btn")
+let btnMenos = document.querySelector("#btn-Menos")
+let linhaRemovida = document.querySelector("#novaLinha")
+
+btnMais.addEventListener('click', function (){
+    linhaRemovida.style.display = "block"
+    btnMais.style.display = "none"
+    btnMenos.style.display = "block"
+});
+
+btnMenos.addEventListener("click", function(){
+    let someLinha = linhaRemovida.style.display = "none"
+    if(someLinha === "none"){
+        btnMenos.style.display = "none"
+        btnMais.style.display = "block"
+    }
+});
+
+/* Botões "+ Mostrar mais" e "- Mostrar menos" da section Bebidas */
+
+let btnMaisDois = document.querySelector("#btn-Mais")
+let btnMenosDois = document.querySelector("#btn-MenosDois")
+let linhaRemovidaDois = document.querySelector("#novaLinhaDois")
+
+btnMaisDois.addEventListener('click', function(){
+    linhaRemovidaDois.style.display = "block"
+    btnMaisDois.style.display = "none"
+    btnMenosDois.style.display = "block"
+});
+
+btnMenosDois.addEventListener('click', function(){
+    let someLinhaDois = linhaRemovidaDois.style.display = "none"
+    if(someLinhaDois === "none"){
+        btnMenosDois.style.display = "none"
+        btnMaisDois.style.display = "block"
+    }
 });
